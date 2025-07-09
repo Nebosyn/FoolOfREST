4fe69b83f71cef36ff469dadf9357a02ad000345
nebosyn <geometrybasic628@gmail.com>
2025-07-09 15:40:26 +0200

    Windows activate.bat path update

--- config_venv.ps1
+++ config_venv.ps1
@@ -1,3 +1,3 @@
 python -m venv .\FoolOfRESTAPI\TelegramLogger\.venv
-.\FoolOfRESTAPI\TelegramLogger\.venv\bin\activate.bat
+.\FoolOfRESTAPI\TelegramLogger\.venv\Scripts\activate.bat
 pip install -r .\FoolOfRESTAPI\TelegramLogger\requirements.txt
